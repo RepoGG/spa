ac09288617b0df80ef7634b57b38e2c957c6b082
RepoGG <grzegorz.godlewski.rk@gmail.com>
2023-07-11 19:27:26 +0200

    project of spa learning

---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,18 @@
+<template>
+   
+        <div>Show</div>
+    <Link href="/">Index Page</Link>
+    
+</template>
+
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+    
+</script>
+
+<script>
+    import MainLayout from '../../Layouts/MainLayout.vue';
+    export default {
+        layout: MainLayout
+    }
+</script>
